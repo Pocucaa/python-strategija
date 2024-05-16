eab33f10ae8cbcad91dfbd3d52b708bd889541f4
Pocucaa <veljkopocuca4@gmail.com>
2024-05-16 14:52:54 +0200

    i

--- fightscrach/CharacterUtilities.py
+++ fightscrach/CharacterUtilities.py
@@ -3,7 +3,7 @@ character_position = [190, 160]
 character_position1 = [230, 180]
 
 class Mercenary:
-    def __init__(self, name, hp=100, at=10, mana=50, mv=5):
+    def __init__(self, name, hp, at, mana, mv):
         self.name = name
         self.hp = hp  # Hit Points
         self.at = at  # Attack
@@ -14,8 +14,9 @@ class Mercenary:
         # self.starty = starty
         # self.x = x  # Starting x position
         # self.y = y  # Starting y position
-        # self.c = (max 5)
+        # self.cb = (max 5)
         # self.type = type (ap ili ad)
+        # self.crit = crit
         # self.ev =
         # self.mb
         # self.pb
@@ -23,27 +24,16 @@ class Mercenary:
     def attack(self, target):
         target.hp -= self.at
         # onhit passive
-        
-    def heal(self, amount):
-        self.hp += amount
+        # crit
 
-    def cast_spell(self, spell_mana_cost, spell_name):
-        if self.mana >= spell_mana_cost:
-            self.mana -= spell_mana_cost
-
-        else:
-            print("Not enough mana to cast the spell.")
-
-    def move(self, distance):
-        if distance <= self.mv:
-            print("Moved {} tiles.".format(distance))
-        else:
-            print("Cannot move that far. Maximum movement range exceeded.")
+    # def move(self, mv, position):
 
 mercenary1 = Mercenary(name="Mercenary1", hp=120, at=8, mana=80, mv=6)
 mercenary2 = Mercenary(name="Mercenarythorns", hp=180, at=14, mana=40, mv=4)
-mercenary11 = Mercenary(name="Mercenary1", hp=121, at=9, mana=81, mv=6)
-mercenary22 = Mercenary(name="Mercenary1", hp=182, at=14, mana=42, mv=4)
+mercenary11 = Mercenary(name="Mercenary11", hp=121, at=9, mana=81, mv=6)
+mercenary22 = Mercenary(name="Mercenary22", hp=182, at=14, mana=42, mv=4)
+
+
 
 # hp 
 # at 
